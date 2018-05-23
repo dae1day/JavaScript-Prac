1d95219b49a3e0d05b3c913372d325929df25d8e
Dae <juliandaeone@gmail.com>
2018-05-23 23:03:06 +1000

    new work

--- DOM.js
+++ DOM.js
@@ -46,3 +46,58 @@ function main() {
     
     $(document).ready(main);
     
+
+    /*
+var skillset = document.getElementsByClassName('skillset');
+
+alert(skillset)
+*/
+
+function main() {
+  
+    $('.skillset').hide();
+      $('.skillset').fadeIn(1000);
+      $('.projects').hide();
+      $('.projects-button').on('click', function(){ 
+        $('.projects').show();
+        $('.projects-button').toggleClass('active');
+      });
+    }
+    
+    $(document).ready(main);
+    
+
+
+
+    ~~~~~~~~~~~~~
+    function main() {
+  
+        $('.skillset').hide();
+          $('.skillset').fadeIn(1000);
+          $('.projects').hide();
+          $('.projects-button').on('click', function(){ 
+            $(this).next().toggle();
+            $(this).toggleClass('active');
+          });
+        }
+        
+        $(document).ready(main);
+        
+
+        --------------------
+
+
+        function main() {
+  
+            $('.skillset').hide();
+              $('.skillset').fadeIn(1000);
+              $('.projects').hide();
+              $('.projects-button').on('click', function(){ 
+                $(this).next().slideToggle(400);
+                $(this).toggleClass('active');
+                $(this).text('Projects Viewed');
+              });
+            }
+            
+            $(document).ready(main);
+            
