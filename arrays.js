23ba4d45935ea1ee44fc5593e5f5097770fd8548
Dae <juliandaeone@gmail.com>
2018-05-25 12:48:01 +1000

    adding arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+let newYearsResolutions = ['get fit', 'code', 'travel'];
+console.log(newYearsResolutions);
+let listItem = newYearsResolutions[0];
+console.log(listItem);
+
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]); // undefined as there are only 3 strings in the array
+
